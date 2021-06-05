6790829bb90c288ea54cfbba4e5e0f8dab92eeb9
joshfeds14 <joshfeds14@gmail,com>
2021-06-05 01:19:06 -0400

    Exercise 11

--- src/main/java/oop/exercise/Exercise11.java
+++ src/main/java/oop/exercise/Exercise11.java
@@ -5,9 +5,20 @@ public class Exercise11
 {
     static Scanner input = new Scanner(System.in);
     public static float euroCount(){
-        System.out.println("How many euros are you exchanging? ");
+        System.out.print("How many euros are you exchanging? ");
+        return input.nextFloat();
+    }
+    public static float exchangeRate(){
+        System.out.print("What is the exchange rate? ");
+        return input.nextFloat();
+    }
+    public static void printRate(float euroQuantity, float euroChangeRate){
+        float USD = euroQuantity * euroChangeRate;
+        System.out.printf("%.2f euros at an exchange rate of %f is %.2f", euroQuantity, euroChangeRate, USD);
     }
     public static void main( String[] args ) {
-
+        float euroQuantity = euroCount();
+        float euroChangeRate = exchangeRate();
+        printRate(euroQuantity, euroChangeRate);
     }
 }
