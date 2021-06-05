a68866fc15e804a8b98a646b4eb20bbefb2535c0
joshfeds14 <joshfeds14@gmail,com>
2021-06-05 01:08:54 -0400

    Exercise 11

---
+++ src/main/java/oop/exercise/Exercise11.java
@@ -0,0 +1,13 @@
+package oop.exercise;
+import java.util.Scanner;
+
+public class Exercise11
+{
+    static Scanner input = new Scanner(System.in);
+    public static float euroCount(){
+        System.out.println("How many euros are you exchanging? ");
+    }
+    public static void main( String[] args ) {
+
+    }
+}
